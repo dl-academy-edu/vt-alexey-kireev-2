36debdb2e3da471f981a1a0b4160712a1d781bc2
Dukalisss <dukalispr@gmail,com>
2021-01-23 15:05:21 +0400

    Добваил страницу Анимация
    добавил страницу с анимацией

--- gulpfile.js
+++ gulpfile.js
@@ -113,8 +113,6 @@ async function buildProject() {
   scss("./src/components/common.scss", `./${finalFolder}`);
   // Обработка картинок
   img("./src/static/images", `./${finalFolder}/static/images`);
-    // Обработка музыки
-    img("./src/static/music", `./${finalFolder}/static/music`);
   // Обработка шрифтов
   fonts("./src/static/fonts", `./${finalFolder}/static/fonts`);
 }
